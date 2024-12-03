ea3909c8228e29d09782659ef50e0f13e68843ed
philipheferreira <philipheferreira@gmail.com>
2024-12-03 16:56:48 -0300

    Feat: teste2 component fail

---
+++ src/app/Components/LogoAngular/angular-logo/angular-logo.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-angular-logo',
+  imports: [],
+  templateUrl: './angular-logo.component.html',
+  styleUrl: './angular-logo.component.css'
+})
+export class AngularLogoComponent {
+
+}
